3f726fe600e11519f6dffc4a9994d247051850c5
piks13 <enriquejavares@gmail.com>
2022-04-25 10:38:17 +0200

    test(HolaLuzExercise): :white_check_mark: master - fix component name

--- test/components/supplyPointSectionSection.spec.js
+++ test/components/supplyPointSectionSection.spec.js
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils'
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Vuetify from 'vuetify'
-import supplyPointSection from '@/components/supplyPointSection.vue'
+import supplyPointSection from '@/components/SupplyPointSection.vue'
 Vue.use(Vuex)
 Vue.use(Vuetify)
 const store = new Vuex.Store({
